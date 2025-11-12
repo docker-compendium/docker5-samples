d2fb40051e7fbe6e13ec9bcbd1fcc7c36208b912
Bernd Oeggl <bernd@webman.at>
2025-11-12 06:56:20 +0000

    update java

--- prog/java/printheadlines.java
+++ prog/java/printheadlines.java
@@ -9,15 +9,15 @@ import java.io.IOException;
 public class printheadlines {
 
   private static String url = "https://heise.de/newsticker/";
-  public static void main(String[] args) throws IOException {
-    System.out.println("Fetching "+ url);
+  void main(String[] args) throws IOException {
+    IO.println("Fetching "+ url);
 
     Document doc = Jsoup.connect(url).get();
     Elements news = doc.select("article.a-article-teaser");
 
-    System.out.println("\nNews: "+ news.size());
+    IO.println("\nNews: "+ news.size());
     for (Element item : news) {
-      System.out.println("* "+ item.text());
+      IO.println("* "+ item.text());
     }
   }
 }
