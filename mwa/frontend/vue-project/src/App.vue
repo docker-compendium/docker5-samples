2e34b82e4e0e6354874fdbe0f100f588cef03ae3
Bernd Oeggl <bernd@webman.at>
2025-11-14 14:59:02 +0000

    style(mwa): npm run format changes from prettier

--- mwa/frontend/vue-project/src/App.vue
+++ mwa/frontend/vue-project/src/App.vue
@@ -48,7 +48,8 @@ body {
 }
 
 #app {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell,
+  font-family:
+    -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell,
     'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
